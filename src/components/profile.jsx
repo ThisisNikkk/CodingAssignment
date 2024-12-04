6dfab31d0935b78f5b59f7c477c3a42a6768ed9b
Nikhil Siwan <developer.nikk@gmail.com>
2024-12-05 00:03:56 +0530

    Error Fix 2

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -18,7 +18,7 @@ function Profile(){
     async function handleLogout() {
         try {
             await auth.signOut();
-            window.location.href = "/login";
+            window.location.href = "/Login";
             console.log("User logged out successfully!");
         } catch (error) {
             console.error("Error logging out:", error.message);
